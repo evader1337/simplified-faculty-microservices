4b1d5b475ac302fa2c709f0d69ea795bf75698c5
Domen <domen.kajdic@gmail.com>
2018-07-06 13:22:57 +0200

    Added check for users and places

--- subjects/src/main/java/com/dk/faculty/subjects/services/beans/SubjectBean.java
+++ subjects/src/main/java/com/dk/faculty/subjects/services/beans/SubjectBean.java
@@ -1,17 +1,36 @@
 package com.dk.faculty.subjects.services.beans;
 
 import com.dk.faculty.subjects.entities.Subject;
+import com.kumuluz.ee.discovery.utils.DiscoveryUtil;
 
+import javax.annotation.PostConstruct;
 import javax.enterprise.context.ApplicationScoped;
+import javax.inject.Inject;
 import javax.persistence.EntityManager;
 import javax.persistence.PersistenceContext;
 import javax.transaction.Transactional;
+import javax.ws.rs.client.Client;
+import javax.ws.rs.client.ClientBuilder;
+import javax.ws.rs.core.Response;
 import java.util.List;
 
 @ApplicationScoped
 public class SubjectBean {
     @PersistenceContext(unitName = "subjects-jpa")
     private EntityManager em;
+    private Client httpClient;
+    private String baseUrlPlaces;
+    private String baseUrlUsers;
+
+    @Inject
+    DiscoveryUtil discoveryUtil;
+
+    @PostConstruct
+    private void init() {
+        httpClient = ClientBuilder.newClient();
+        baseUrlPlaces = discoveryUtil.getServiceInstance("places", "1.0.0", "dev").get().toString() + "/v1";
+        baseUrlUsers = discoveryUtil.getServiceInstance("users", "1.0.0", "dev").get().toString() + "/v1";
+    }
 
     public List<Subject> getSubjects() {
         return em.createQuery("Select s from Subject s").getResultList();
@@ -21,9 +40,29 @@ public class SubjectBean {
         return em.find(Subject.class, id);
     }
 
+    public boolean checkForPlacesAndUsers(Subject s) {
+        if(s.getPlace() != null) {
+            Response r =  httpClient
+                    .target(baseUrlPlaces).path("places").path("{id}").resolveTemplate("id", s.getPlace())
+                    .request().get();
+            if(r.getStatusInfo().getStatusCode() == 404) {
+                return false;
+            }
+        }
+        if(s.getProfessor() != null) {
+            Response r =  httpClient
+                    .target(baseUrlUsers).path("professors").path("{id}").resolveTemplate("id", s.getProfessor())
+                    .request().get();
+            if(r.getStatusInfo().getStatusCode() == 404) {
+                return false;
+            }
+        }
+        return true;
+    }
+
     @Transactional
     public Subject addSubject(Subject subject) {
-        //preveri če obstaja prostor in profesor
+        if(!checkForPlacesAndUsers(subject)) return null;
         if(subject != null) {
             em.persist(subject);
             return subject;
@@ -44,7 +83,7 @@ public class SubjectBean {
 
     @Transactional
     public Subject updateSubject(Integer id, Subject subject) {
-        //preveri če obstaja prostor in profesor
+        if(!checkForPlacesAndUsers(subject)) return null;
         Subject s = getSubject(id);
         subject.setID(s.getID());
         em.merge(subject);
