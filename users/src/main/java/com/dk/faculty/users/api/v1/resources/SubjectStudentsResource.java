2bbb04ad25bfdfc9ec7da9004c81dff963c27345
Domen <domen.kajdic@gmail.com>
2018-07-07 12:28:39 +0200

    New renamed endpoint

---
+++ users/src/main/java/com/dk/faculty/users/api/v1/resources/SubjectStudentsResource.java
@@ -0,0 +1,29 @@
+package com.dk.faculty.users.api.v1.resources;
+
+import com.dk.faculty.users.services.beans.StudentBean;
+
+import javax.enterprise.context.ApplicationScoped;
+import javax.inject.Inject;
+import javax.ws.rs.*;
+import javax.ws.rs.core.MediaType;
+import javax.ws.rs.core.Response;
+import java.util.List;
+
+@ApplicationScoped
+@Path("subject-students")
+@Produces(MediaType.APPLICATION_JSON)
+@Consumes(MediaType.APPLICATION_JSON)
+public class SubjectStudentsResource {
+    @Inject
+    private StudentBean studentBean;
+
+    @Path("{id}")
+    @GET
+    public Response getStudentSubjects(@PathParam("id") Integer subjectId) {
+        List<Integer> subjectStudents = studentBean.getStudentSubjects(subjectId);
+        if(subjectStudents != null) {
+            return Response.status(Response.Status.OK).entity(subjectStudents).build();
+        }
+        throw new BadRequestException("Provided invalid fields. Please check your input.");
+    }
+}
